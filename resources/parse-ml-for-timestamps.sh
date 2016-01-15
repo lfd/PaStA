ecbc542a94107113764f3b0997159c373f7e6c1b
Ralf Ramsauer <ralf@ramses-pyramidenbau.de>
2016-01-15 16:19:19 +0100

    changes parse-ml-for-timestamps.sh and manually created new timestamp list
    Now the timestamps are more accurate, as Mailinglists were parsed in
    order to retreive the releasedate information of the rt version. If
    there was no announce on the Mailinglist, this information was taken
    from the latest modify timestamp of all files inside a quilt patchset.
    
    Signed-off-by: Ralf Ramsauer <ralf@ramses-pyramidenbau.de>

--- resources/parse-ml-for-timestamps.sh
+++ resources/parse-ml-for-timestamps.sh
@@ -4,14 +4,16 @@
 #rm $1
 #nntp-pull --server=news.gmane.org -v --reget $1
 
-cat gmane.linux.rt.user | \
-	grep -i -Pzo "^Subject: \[Announce.*\-rt.*\nDate:.*" -A1 | \
+#grep -i -Pzo "^Subject: \[Announce.*\-rt.*\nDate:.*" -A1 | \
+
+cat $1 | \
+	grep -i -Pzo "^Subject: .*\-rt.*\nDate:.*" -A1 | \
 	sed -e 's/^Date: //' | \
 	sed -e 's/Subject: \[.*\] //' > sum
 
 cat sum | awk 'NR % 2 == 1' > versions
-cat sum | awk 'NR % 2 == 0' | xargs -n 1 -I Z date --date="Z" +"%s \"%F %R\"" > dates
+cat sum | awk 'NR % 2 == 0' | xargs -n 1 -I Z date --date="Z" +"\"%F\"" > dates
 
-paste versions dates | sort -V > releaseDateList
+paste versions dates > ${1}.releaseDateList
 
 rm dates sum versions
