c40e77608350f2a02b12afff15686b5c597a14fc
Ralf Ramsauer <ralf@ramses-pyramidenbau.de>
2016-01-28 17:48:49 +0100

    PatchStack.py: Improvements
    - Improved optimize()
    - Added iterator
    
    Signed-off-by: Ralf Ramsauer <ralf@ramses-pyramidenbau.de>

--- PatchStack.py
+++ PatchStack.py
@@ -225,16 +225,22 @@ class TransitiveKeyList:
         self.transitive_list = []
 
     def optimize(self):
-        # Reset lookup table
-        self.forward_lookup = {}
 
-        # Filter orphaned elements
-        self.transitive_list = list(filter(None, self.transitive_list))
+        # Get optimized list
+        filtered_list = list(filter(None, self.transitive_list))
 
-        # Recreate the forward lookup dictionary
-        for i, keylist in enumerate(self.transitive_list):
-            for key in keylist:
-                self.forward_lookup[key] = i
+        # Check if optimization is necessary
+        if len(self.transitive_list) != len(filtered_list):
+            # Reset lookup table
+            self.forward_lookup = {}
+
+            # Filter orphaned elements
+            self.transitive_list = filtered_list
+
+            # Recreate the forward lookup dictionary
+            for i, keylist in enumerate(self.transitive_list):
+                for key in keylist:
+                    self.forward_lookup[key] = i
 
     def is_related(self, key1, key2):
         if key1 in self.forward_lookup and key2 in self.forward_lookup:
@@ -285,6 +291,11 @@ class TransitiveKeyList:
         with open(filename, 'w') as f:
             f.write(str(self))
             f.close()
+
+    def __iter__(self):
+        self.optimize()
+        for i in self.transitive_list:
+            yield i
 
     def __str__(self):
         return '\n'.join(
