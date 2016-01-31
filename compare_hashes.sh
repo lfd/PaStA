616f0fe868ea33d9fe7be71f35770f2e4c8ff7b1
Ralf Ramsauer <ralf@ramses-pyramidenbau.de>
2016-01-31 20:04:33 +0100

    Added compare_hashes tool
    Signed-off-by: Ralf Ramsauer <ralf@ramses-pyramidenbau.de>

---
+++ compare_hashes.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+repo="./linux"
+
+function show_hash {
+	tmp=$(mktemp)
+	git -C $repo --no-pager show $1 > $tmp
+	echo $tmp
+}
+
+lhs=$(show_hash $1)
+rhs=$(show_hash $2)
+pr -w 150 -m -t $lhs $rhs
+rm $rhs $lhs
