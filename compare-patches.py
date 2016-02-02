16bc9e03a50df7b927aa421e2c9df55149834ed2
Ralf Ramsauer <ralf@ramses-pyramidenbau.de>
2016-02-02 17:28:49 +0100

    compare-patches.py: Added git output
    Signed-off-by: Ralf Ramsauer <ralf@ramses-pyramidenbau.de>

--- compare-patches.py
+++ compare-patches.py
@@ -3,9 +3,12 @@
 import sys
 
 from PatchEvaluation import preevaluate_single_patch, evaluate_single_patch
+from Tools import compare_hashes
 
 commit_a = sys.argv[1]
 commit_b = sys.argv[2]
+
+compare_hashes(commit_a, commit_b)
 
 retval = preevaluate_single_patch(commit_a, commit_b)
 #retval = True
@@ -13,9 +16,9 @@ if retval:
     print('Preevaluation: Possible candidates')
     retval = evaluate_single_patch(commit_a, commit_b)
     if retval is None:
-        print('Rating: 0')
+        print('Rating: None')
     else:
         hash, rating, message = retval
-        print('Rating: ' + str(rating) + ' ' + message)
+        print('Rating: ' + str(rating) + ' || ' + message)
 else:
     print('Preevaluation: Not related')
