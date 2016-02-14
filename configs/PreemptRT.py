3feca1d6bca3bad11d86912f4019f6eec9b35222
Ralf Ramsauer <ralf@ramses-pyramidenbau.de>
2016-02-14 20:01:03 +0100

    configs: Add PreemptRT config
    Signed-off-by: Ralf Ramsauer <ralf@ramses-pyramidenbau.de>

---
+++ configs/PreemptRT.py
@@ -0,0 +1,11 @@
+PROJECT_NAME = 'PreemptRT'
+
+REPO_LOCATION = './PSA-resources/PreemptRT/linux'
+LOG_LOCATION = './PSA-resources/PreemptRT/log'
+
+PATCH_STACK_DEFINITION = 'PSA-resources/PreemptRT/resources/patch-stack-definition.dat'
+
+BRANCH_PREFIX = 'preemptrt-'
+
+COMMITCOUNT_PREFIX = 'commitcount-preemptrt-'
+GNUPLOT_PREFIX = './plots/'
