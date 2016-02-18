12696862786d550270950124fd81f5cb2f65ec38
Ralf Ramsauer <ralf@ramses-pyramidenbau.de>
2016-02-18 12:48:24 +0100

    interactive rating: bugfix
    Signed-off-by: Ralf Ramsauer <ralf@ramses-pyramidenbau.de>

--- interactive_rating.py
+++ interactive_rating.py
@@ -6,7 +6,6 @@ from os import path
 from config import *
 from Tools import DictList, EvaluationResult, TransitiveKeyList
 
-SIMILAR_PATCHES_FILE = './similar_patch_list'
 EVALUATION_RESULT_FILE = './evaluation-result.pkl'
 INTERACTIVE_THRESHOLD = 0.80
 AUTOACCEPT_THRESHOLD = 0.9
