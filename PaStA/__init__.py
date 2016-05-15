a1495025dbe1276eb11a918def900e0fc92b00bd
Ralf Ramsauer <ralf@ramses-pyramidenbau.de>
2016-05-15 22:13:44 +0200

    Simplify import statements
    Signed-off-by: Ralf Ramsauer <ralf@ramses-pyramidenbau.de>

--- PaStA/__init__.py
+++ PaStA/__init__.py
@@ -8,3 +8,13 @@ repo = Repository(config.repo_location)
 
 from PaStA.PatchStack import PatchStackDefinition
 patch_stack_definition = PatchStackDefinition.parse_definition_file(config.patch_stack_definition)
+
+# Import statements
+from PaStA.EquivalenceClass import EquivalenceClass
+from PaStA.PatchEvaluation import DictList, EvaluationResult, EvaluationType, evaluate_patch_list, SimRating,\
+    compare_hashes, preevaluate_single_patch, evaluate_single_patch, getch
+
+from PaStA.Config import Thresholds
+
+from PaStA import config, patch_stack_definition
+from PaStA.PatchStack import cache_commit_hashes, get_commit, format_date_ymd
