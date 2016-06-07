baac75a8ca97bf46210b4250ec3824fc35aef034
Ralf Ramsauer <ralf.ramsauer@othr.de>
2016-06-07 17:17:11 +0200

    Patch: fix regular expression
    Older patches can have an optional timestamp at the end, like:
    
    --- linux-2.5-devel/arch/i386/kernel/apic.c	17 Apr 2003 23:28:48 -0000	1.40
    +++ linux-2.5-devel/arch/i386/kernel/apic.c	19 May 2003 07:07:52 -0000
    
    The new regex will drop everything after the first whitespace character.
    
    Signed-off-by: Ralf Ramsauer <ralf.ramsauer@othr.de>

--- PaStA/Patch.py
+++ PaStA/Patch.py
@@ -43,8 +43,8 @@ class Diff:
     DIFF_SELECTOR_REGEX = re.compile(r'^[-\+@]')
 
     # The two-line unified diff headers
-    FILE_SEPARATOR_MINUS_REGEX = re.compile(r'^--- (.+)$')
-    FILE_SEPARATOR_PLUS_REGEX = re.compile(r'^\+\+\+ (.+)$')
+    FILE_SEPARATOR_MINUS_REGEX = re.compile(r'^--- ([^\s]*).*$')
+    FILE_SEPARATOR_PLUS_REGEX = re.compile(r'^\+\+\+ ([^\s]*).*$')
 
     # Exclude '--cc' diffs
     EXCLUDE_CC_REGEX = re.compile(r'^diff --cc (.+)$')
