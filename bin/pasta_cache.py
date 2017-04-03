d07cd7ebe74db4c66f494dac974326db10a1956e
Ralf Ramsauer <ralf.ramsauer@oth-regensburg.de>
2017-04-03 14:59:39 +0200

    pasta_cache: enforce coding style
    Signed-off-by: Ralf Ramsauer <ralf.ramsauer@oth-regensburg.de>

--- bin/pasta_cache.py
+++ bin/pasta_cache.py
@@ -3,37 +3,45 @@
 """
 PaStA - Patch Stack Analysis
 
-Copyright (c) OTH Regensburg, 2016
+Copyright (c) OTH Regensburg, 2016-2017
 
 Author:
-  Ralf Ramsauer <ralf.ramsauer@othr.de>
+  Ralf Ramsauer <ralf.ramsauer@oth-regensburg.de>
 
 This work is licensed under the terms of the GNU GPL, version 2.  See
 the COPYING file in the top-level directory.
 """
+
 import datetime
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__),
+                                                '..')))
 from PaStA import *
 
 
 def cache(config, prog, argv):
-    parser = argparse.ArgumentParser(prog=prog, description='create commit cache')
+    parser = argparse.ArgumentParser(prog=prog,
+                                     description='create commit cache')
 
     parser.add_argument('-stack', action='store_true', default=False,
                         help='create cache for commits on patch stacks')
     parser.add_argument('-upstream', action='store_true', default=False,
                         help='create cache for upstream commits')
     parser.add_argument('-all', action='store_true', default=False,
-                        help='create cache for upstream and patch stack commits')
+                        help='create cache for upstream and patch stack '
+                             'commits')
     parser.add_argument('-mbox', metavar='filename', default=None,
                         help='create cache for mailbox')
-    parser.add_argument('-mindate', dest='mindate', metavar='mindate', default=config.mbox_mindate,
-                        help='Skip mails older than mindate (only together with -mbox, default: %(default)s)')
-    parser.add_argument('-maxdate', dest='maxdate', metavar='maxdate', default=config.mbox_maxdate,
-                        help='Skip mails older than mindate (only together with -mbox, default: %(default)s)')
+    parser.add_argument('-mindate', dest='mindate', metavar='mindate',
+                        default=config.mbox_mindate,
+                        help='Skip mails older than mindate '
+                             '(only together with -mbox, default: %(default)s)')
+    parser.add_argument('-maxdate', dest='maxdate', metavar='maxdate',
+                        default=config.mbox_maxdate,
+                        help='Skip mails older than mindate '
+                             '(only together with -mbox, default: %(default)s)')
 
     args = parser.parse_args(argv)
 
@@ -55,7 +63,8 @@ def cache(config, prog, argv):
         repo.export_commit_cache(config.commit_cache_upstream_filename)
         repo.clear_commit_cache()
     if args.mbox:
-        ids = get_commits_from_file(config.mailbox_id_filename, ordered=False, must_exist=False)
+        ids = get_commits_from_file(config.mailbox_id_filename, ordered=False,
+                                    must_exist=False)
         mindate = datetime.datetime.strptime(args.mindate, "%Y-%m-%d")
         maxdate = datetime.datetime.strptime(args.maxdate, "%Y-%m-%d")
         ids |= load_and_cache_mbox(repo, args.mbox, mindate, maxdate)
