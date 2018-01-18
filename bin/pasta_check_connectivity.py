7f325a9cc5c7d8348ec3e24c5fbbb46d4a199ef8
Ralf Ramsauer <ralf.ramsauer@oth-regensburg.de>
2018-01-18 15:52:55 +0100

    pasta: implement check_connectivity
    This tool checks if all elements of a pasta result are still reachable.
    
    Signed-off-by: Ralf Ramsauer <ralf.ramsauer@oth-regensburg.de>

---
+++ bin/pasta_check_connectivity.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+
+"""
+PaStA - Patch Stack Analysis
+
+Copyright (c) OTH Regensburg, 2018
+
+Author:
+  Ralf Ramsauer <ralf.ramsauer@oth-regensburg.de>
+
+This work is licensed under the terms of the GNU GPL, version 2.  See
+the COPYING file in the top-level directory.
+"""
+
+import os
+import sys
+
+from logging import getLogger
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from PaStA import *
+
+log = getLogger(__name__[-15:])
+
+
+def check_connectivity(config, prog, argv):
+    parser = argparse.ArgumentParser(prog=prog,
+                                     description='Check result connectivity')
+
+    # boolean switch to chose mailbox result
+    parser.add_argument('-mbox', dest='mbox', default=False,
+                        action='store_true')
+
+    parser.add_argument('-d', dest='d', default=False, action='store_true')
+
+    args = parser.parse_args(argv)
+
+    repo = config.repo
+
+    f_patch_groups, patch_groups = config.load_patch_groups(args.mbox, True)
+
+    keys = patch_groups.get_keys()
+    for elem in keys:
+        if elem not in repo:
+            if args.d:
+                log.info('Woof woof, removing: %s' % elem)
+                patch_groups.remove_key(elem)
+            else:
+                log.info('Woof woof, not reachable: %s' % elem)
+
+    if args.d:
+        patch_groups.optimize()
+        patch_groups.to_file(f_patch_groups)
+
+
+if __name__ == '__main__':
+    config = Config(sys.argv[1])
+    check_connectivity(config, sys.argv[0], sys.argv[2:])
