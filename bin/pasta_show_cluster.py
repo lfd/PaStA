a13cf81d28e34acc431dc6522e7724a47b6aaf6e
Ralf Ramsauer <ralf.ramsauer@oth-regensburg.de>
2018-07-31 13:39:18 +0200

    New tool: pasta show_cluster
    Shows a single cluster of a clustering. May be parameterised with -mbox.
    
    Signed-off-by: Ralf Ramsauer <ralf.ramsauer@oth-regensburg.de>

---
+++ bin/pasta_show_cluster.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+
+"""
+PaStA - Patch Stack Analysis
+
+Copyright (c) OTH Regensburg, 2018
+
+Author:
+  Ralf Ramsauer <ralf.ramsauer@oth-regensburg.de>
+
+This work is licensed under the terms of the GNU GPL, version 2.  See
+the COPYING file in the top-level directory.
+"""
+
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__),
+                                                '..')))
+from pypasta import *
+
+
+def show_cluster(config, prog, argv):
+    parser = argparse.ArgumentParser(prog=prog, description='compare patches')
+
+    parser.add_argument('-n', action='store_false', default=True,
+                        help = 'don\'t use a pager')
+    parser.add_argument('patch', metavar='representative', type=str,
+                        help='Commit hash / Mail ID')
+    parser.add_argument('-mbox', dest='mbox', default=False,
+                        action='store_true')
+
+    args = parser.parse_args(argv)
+    repo = config.repo
+
+    _, patch_groups = config.load_patch_groups(args.mbox, must_exist=True)
+
+    cluster = list(patch_groups[args.patch])
+
+    if len(cluster) == 1:
+        show_commit(repo, cluster[0])
+        return
+
+    for i in range(len(cluster)-1):
+        patch_a = cluster[i]
+        patch_b = cluster[i+1]
+
+        show_commits(repo, patch_a, patch_b, args.n)
+
+        # evaluation type plays no role in this case
+        rating = evaluate_commit_list(repo, config.thresholds,
+                                      False, None,
+                                      [patch_a], [patch_b])
+        if rating:
+            print(rating[patch_a][0][1])
+        else:
+            print('Not related')
+        getch()
+
+
+if __name__ == '__main__':
+    config = Config(sys.argv[1])
+    show_cluster(config, sys.argv[0], sys.argv[2:])
