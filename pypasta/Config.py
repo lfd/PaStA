lfd/PaStA

commit 945dbaa9b151ea2b2db77d2fda524abec2e29bc9

pasta_sync: rework caching
Don't write cache files if nothing changed. Use helper functions in the Config
class to perform loads and updates of ccaches.

Signed-off-by: Ralf Ramsauer <ralf.ramsauer@oth-regensburg.de>

pypasta/Config.py
@@ -194,6 +194,36 @@ class Config:
     def project_root(self):
         return self._project_root
 
+    def load_ccache_upstream(self):
+        self.repo.load_ccache(self.f_ccache_upstream)
+
+    def load_ccache_stack(self):
+        self.repo.load_ccache(self.f_ccache_stack)
+
+    def load_ccache_mbox(self):
+        self.repo.load_ccache(self.f_ccache_mbox)
+
+    def _update_ccache(self, f_ccache, commits, desc):
+        repo = self.repo
+        repo.clear_commit_cache()
+        repo.load_ccache(f_ccache, desc)
+        cached = repo.cache_commits(commits)
+        if cached:
+            repo.export_ccache(f_ccache)
+        repo.clear_commit_cache()
+
+    def update_ccache_upstream(self):
+        self._update_ccache(self.f_ccache_upstream, self.upstream_hashes,
+                            'upstream')
+
+    def update_ccache_mbox(self):
+        self._update_ccache(self.f_ccache_mbox, self.repo.mbox.message_ids(),
+                            'mbox')
+
+    def update_ccache_stack(self):
+        self._update_ccache(self.f_ccache_stack, self.psd.commits_on_stacks,
+                            'stack')
+
     def load_patch_groups(self, must_exist=True, f_patch_groups=None):
         if f_patch_groups is None:
             f_patch_groups = self.f_pasta_result

bin/pasta_analyse.py
@@ -149,7 +149,7 @@ def analyse(config, prog, argv):
                   format_date_ymd(mbox_time_window[1])))
         # load mbox ccache very early, because we need it in any case if it
         # exists.
-        repo.load_ccache(config.f_ccache_mbox)
+        config.load_ccache_mbox()
         victims = config.repo.mbox.message_ids(mbox_time_window)
 
         # we have to temporarily cache those commits to filter out invalid
@@ -184,7 +184,7 @@ def analyse(config, prog, argv):
         repo = config.repo
         _repo = repo
 
-        repo.load_ccache(config.f_ccache_stack)
+        config.load_ccache_stack()
 
         evaluation_list = []
         for patch_stack in psd:
@@ -243,7 +243,7 @@ def analyse(config, prog, argv):
             else:
                 candidates = set(config.upstream_hashes)
 
-            repo.load_ccache(config.f_ccache_upstream)
+            config.load_ccache_upstream()
 
             # cache missing commits
             repo.cache_commits(representatives | candidates)

bin/pasta_patch_descriptions.py
@@ -73,7 +73,7 @@ def patch_descriptions(config, prog, argv):
     patch_groups = config.patch_groups
 
     # we can at least cache all commits on the patch stacks
-    repo.load_ccache(config.f_ccache_stack)
+    config.load_ccache_stack()
 
     # iterate over everything, including upstream commits
     all_commit_hashes = []

bin/pasta_statistics.py
@@ -69,7 +69,7 @@ def statistics(config, prog, argv):
         # commits of the patch stacks, as we need date information of each
         # of them
         if args.date_selector != 'SRD':
-            repo.load_ccache(config.ccache_stack_filename)
+            config.load_ccache_stack()
             repo.cache_commits(psd.commits_on_stacks)
 
         # Export information of patch groups

bin/pasta_sync.py
@@ -94,20 +94,11 @@ def sync(config, prog, argv):
         remove_if_exist(config.f_mail_thread_cache)
 
     if create_stack:
-        repo.load_ccache(config.f_ccache_stack)
-        repo.cache_commits(config.psd.commits_on_stacks)
-        repo.export_ccache(config.f_ccache_stack)
-        repo.clear_commit_cache()
+        config.update_ccache_stack()
     if create_upstream:
-        repo.load_ccache(config.f_ccache_upstream)
-        repo.cache_commits(config.upstream_hashes)
-        repo.export_ccache(config.f_ccache_upstream)
-        repo.clear_commit_cache()
+        config.update_ccache_upstream()
     if create_mbox:
-        repo.load_ccache(config.f_ccache_mbox)
-        repo.cache_commits(repo.mbox.message_ids())
-        repo.export_ccache(config.f_ccache_mbox)
-        repo.clear_commit_cache()
+        config.update_ccache_mbox()
 
         # Update the mail thread cache
         repo.mbox.load_threads()

bin/pasta_upstream_duration.py
@@ -82,9 +82,9 @@ def upstream_duration(config, prog, argv):
     _, patch_groups = config.load_patch_groups()
 
     if args.mbox:
-        repo.load_ccache(config.f_ccache_mbox)
+        config.load_ccache_mbox()
     else:
-        repo.load_ccache(config.f_ccache_stack)
+        config.load_ccache_stack()
 
     log.info('Starting evaluation.')
     pool = Pool(cpu_count())

pypasta/Repository/Repository.py
@@ -125,8 +125,8 @@ class Repository:
 
         return commit
 
-    def load_ccache(self, f_ccache):
-        log.info('Loading commit cache file %s' % f_ccache)
+    def load_ccache(self, f_ccache, description):
+        log.info('Loading %s commit cache' % description)
         try:
             with open(f_ccache, 'rb') as f:
                 this_commits = pickle.load(f)
@@ -164,7 +164,7 @@ class Repository:
         worklist = commit_hashes - already_cached
 
         if len(worklist) == 0:
-            return commit_hashes
+            return set()
 
         log.info('Caching %d/%d commits' % (len(worklist), len(commit_hashes)))
 
@@ -190,7 +190,7 @@ class Repository:
 
         self._inject_commits(result)
 
-        return commit_hashes - invalid
+        return set(result.keys())
 
     def __getitem__(self, item):
         return self.get_commit(item)

commit 8ee53acf8b32dacaba64038884a519d19878c8a0

Config: fix bug in load_ccache
load_ccache requires a description argument

Fixes: 945dbaa ("pasta_sync: rework caching")
Signed-off-by: Ralf Ramsauer <ralf.ramsauer@oth-regensburg.de>

pypasta/Config.py
@@ -196,13 +196,13 @@ class Config:
         return self._project_root
 
     def load_ccache_upstream(self):
-        self.repo.load_ccache(self.f_ccache_upstream)
-
-    def load_ccache_stack(self):
-        self.repo.load_ccache(self.f_ccache_stack)
+        self.repo.load_ccache(self.f_ccache_upstream, 'upstream')
 
     def load_ccache_mbox(self):
-        self.repo.load_ccache(self.f_ccache_mbox)
+        self.repo.load_ccache(self.f_ccache_mbox, 'mbox')
+
+    def load_ccache_stack(self):
+        self.repo.load_ccache(self.f_ccache_stack, 'stack')
 
     def _update_ccache(self, f_ccache, commits, desc):
         repo = self.repo