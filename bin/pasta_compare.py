lfd/PaStA

commit 77a7c394ac5b56e79d141db1e4d962e9474bd5c3

Repository: refactor mbox_register to register_mbox
No functional change.

Signed-off-by: Ralf Ramsauer <ralf.ramsauer@oth-regensburg.de>

bin/pasta_compare.py
@@ -40,7 +40,7 @@ def compare(config, prog, argv):
     repo = config.repo
 
     if any([x.startswith('<') for x in commits]):
-        repo.mbox_register(config.d_mbox)
+        repo.register_mbox(config.d_mbox)
 
     if len(commits) == 1:
         show_commit(repo, commits[0])

bin/pasta_sync.py
@@ -73,7 +73,7 @@ def sync(config, prog, argv):
         config.load_upstream_hashes(force_reload=True)
 
         if args.mbox and config.mode == Config.Mode.MBOX:
-            config.repo.mbox_update(config)
+            config.repo.update_mbox(config)
 
     if args.clear is None and args.create is None:
         args.create = 'all'
@@ -99,7 +99,7 @@ def sync(config, prog, argv):
         repo.export_ccache(config.f_ccache_upstream)
         repo.clear_commit_cache()
     if create_mbox:
-        config.repo.mbox_register(config)
+        config.repo.register_mbox(config)
 
         repo.load_ccache(config.f_ccache_mbox)
         repo.cache_commits(repo.mbox.message_ids())

pypasta/Config.py
@@ -197,7 +197,7 @@ class Config:
         if f_patch_groups is None:
             f_patch_groups = self.f_pasta_result
             if self.mode == Config.Mode.MBOX:
-                self.repo.mbox_register(self)
+                self.repo.register_mbox(self)
                 f_patch_groups = self.f_mbox_result
 
         if must_exist:

pypasta/Repository/Repository.py
@@ -232,16 +232,16 @@ class Repository:
         # preserve order
         return list(reversed(inserted_cherries))
 
-    def mbox_register(self, config):
+    def register_mbox(self, config):
         if not self.mbox:
             self.mbox = Mbox(config)
 
-    def mbox_update(self, config):
-        self.mbox_register(config)
+    def update_mbox(self, config):
+        self.register_mbox(config)
         self.mbox.update()
 
         # The mbox doesn't track changes after an update. The easiest
         # workaround is to reload the whole instance.
         del self.mbox
         self.mbox = None
-        self.mbox_register(config)
+        self.register_mbox(config)

commit 7dd5a6fb9f19f0e00e86c42b771bb79f0185a4ef

pasta_compare: fix bug
API usage changed, and I forgot to align mailbox registration in
pasta_compare.

Fixes: 77a7c394a ("Repository: refactor mbox_register to register_mbox")
Signed-off-by: Ralf Ramsauer <ralf.ramsauer@oth-regensburg.de>

bin/pasta_compare.py
@@ -38,7 +38,7 @@ def compare(config, prog, argv):
     repo = config.repo
 
     if any([x.startswith('<') for x in commits]):
-        repo.register_mbox(config.d_mbox)
+        repo.register_mbox(config)
 
     if len(commits) == 1:
         show_commit(repo, commits[0])