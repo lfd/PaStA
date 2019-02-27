lfd/PaStA

commit 313d29649e9a12b431215f1eed3cacd6f6a95ae0

PatchEvaluation: New parameter: author_date_interval
This is a parameter used for analysation.

It denotes the maximum temporal distance (in days) for two patches by
subtracting their author dates.

0 days means 'infinite distance'.

Signed-off-by: Ralf Ramsauer <ralf.ramsauer@oth-regensburg.de>

bin/pasta_ripup.py
@@ -63,6 +63,10 @@ def ripup(config, prog, argv):
                         default=config.thresholds.filename, type=float,
                         help='Minimum filename similarity '
                              '(default: %(default)s)')
+    parser.add_argument('-adi', dest='thres_adi', metavar='days', type=int,
+                        default=config.thresholds.author_date_interval,
+                        help='Author date interval (default: %(default)s)')
+
 
     args = parser.parse_args(argv)
     representatives = args.reps
@@ -73,7 +77,8 @@ def ripup(config, prog, argv):
                                    args.thres_diff_lines,
                                    args.thres_heading,
                                    args.thres_filename,
-                                   args.weight)
+                                   args.weight,
+                                   args.adi)
 
     f_patch_groups, patch_groups = config.load_patch_groups(args.mbox, True)
 

bin/pasta_analyse.py
@@ -100,6 +100,9 @@ def analyse(config, prog, argv):
     parser.add_argument('-dlr', dest='thres_diff_lines', metavar='threshold',
                         type=float, default=config.thresholds.diff_lines_ratio,
                         help='Diff lines ratio threshold (default: %(default)s)')
+    parser.add_argument('-adi', dest='thres_adi', metavar='days', type=int,
+                        default=config.thresholds.author_date_interval,
+                        help='Author date interval (default: %(default)s)')
 
     parser.add_argument('-er', dest='er_filename', metavar='filename',
                         default=config.f_evaluation_result,
@@ -145,6 +148,7 @@ def analyse(config, prog, argv):
     config.thresholds.heading = args.thres_heading
     config.thresholds.filename = args.thres_filename
     config.thresholds.diff_lines_ratio = args.thres_diff_lines
+    config.thresholds.author_date_interval = args.thres_adi
 
     repo = config.repo
     mbox = args.mbox

pypasta/Config.py
@@ -25,7 +25,8 @@ log = getLogger(__name__[-15:])
 
 class Thresholds:
     def __init__(self, autoaccept, interactive, diff_lines_ratio,
-                 heading, filename, message_diff_weight):
+                 heading, filename, message_diff_weight,
+                 author_date_interval):
         """
         :param autoaccept: Auto accept threshold. Ratings with at least this
                threshold will automatically be accepted.
@@ -39,6 +40,10 @@ class Thresholds:
                (files in a repo may move).
         :param message_diff_weight: heuristic factor of message rating to diff
                rating
+        :param author_date_interval: Used for preevaluation: Two patches will only
+               be considered for comparison, if the difference of their
+               author_dates is within patch_time_window days. A value of 0
+               means infinite days.
         """
 
         # t_a
@@ -51,8 +56,10 @@ class Thresholds:
         self.filename = filename
         # w
         self.message_diff_weight = message_diff_weight
-
+        # dlr
         self.diff_lines_ratio = diff_lines_ratio
+        # ptw
+        self.author_date_interval = author_date_interval
 
 
 class Config:
@@ -146,7 +153,8 @@ class Config:
                                      float(pasta.get('DIFF_LINES_RATIO')),
                                      float(pasta.get('HEADING_THRESHOLD')),
                                      float(pasta.get('FILENAME_THRESHOLD')),
-                                     float(pasta.get('MESSAGE_DIFF_WEIGHT')))
+                                     float(pasta.get('MESSAGE_DIFF_WEIGHT')),
+                                     int(pasta.get('AUTHOR_DATE_INTERVAL')))
 
         self.patch_stack_definition = \
             PatchStackDefinition.parse_definition_file(self)

pypasta/PatchEvaluation.py
@@ -523,6 +523,16 @@ def preevaluate_commit_list(repo, thresholds, left_hashes, right_hashes, paralle
                     this_right_hashes |= right_files[affect]
             # no comparisons against each other
             this_right_hashes.discard(left_hash)
+
+            # respect author_date_interval. Only consider patches for
+            # comparison that have at max a temporal author_date
+            # distance of author_date_interval days
+            left_author_date = repo[left_hash].author_date
+            if thresholds.author_date_interval:
+               this_right_hashes = {
+                    x for x in this_right_hashes
+                    if abs((repo[x].author_date - left_author_date).days) <
+                       thresholds.author_date_interval}
             if len(this_right_hashes):
                 preeval_result[left_hash] = this_right_hashes
         return preeval_result

commit b4f1da5d21957cf8c200f61752c9da4f74ea4e1c

pasta_ripup: fix bug
Fixes an old bug that was introduced during refactoring.

Fixes: 313d29649e ("PatchEvaluation: New parameter: author_date_interval")
Signed-off-by: Ralf Ramsauer <ralf.ramsauer@oth-regensburg.de>

bin/pasta_ripup.py
@@ -1,7 +1,7 @@
 """
 PaStA - Patch Stack Analysis
 
-Copyright (c) OTH Regensburg, 2016-2018
+Copyright (c) OTH Regensburg, 2016-2019
 
 Author:
   Ralf Ramsauer <ralf.ramsauer@oth-regensburg.de>
@@ -64,6 +64,7 @@ def ripup(config, prog, argv):
     args = parser.parse_args(argv)
     representatives = args.reps
     repo = config.repo
+    mbox = config.mode == Config.Mode.MBOX
 
     config.thresholds = Thresholds(args.thres_accept,
                                    args.thres_interactive,
@@ -71,7 +72,7 @@ def ripup(config, prog, argv):
                                    args.thres_heading,
                                    args.thres_filename,
                                    args.weight,
-                                   args.adi)
+                                   args.thres_adi)
 
     f_patch_groups, patch_groups = config.load_patch_groups()
 
@@ -83,9 +84,8 @@ def ripup(config, prog, argv):
         elems = patch_groups.ripup_cluster(representative)
 
         evaluation_result = evaluate_commit_list(repo, config.thresholds,
-                                                 args.mbox,
+                                                 mbox,
                                                  EvaluationType.PatchStack,
-                                                 patch_groups,
                                                  elems, elems,
                                                  parallelise=False,
                                                  verbose=True,