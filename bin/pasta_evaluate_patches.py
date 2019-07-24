e6372751729ef8bce10163b3beec96d9c16e755c
Sebastian Duda <git@sebdu.de>
2019-07-24 10:35:57 +0200

    Add script to evaluate patches
    Signed-off-by: Sebastian Duda <sebastian.duda@fau.de>

---
+++ bin/pasta_evaluate_patches.py
@@ -0,0 +1,14 @@
+"""
+PaStA - Patch Stack Analysis
+
+Copyright (c) Bayerische Motoren Werke Aktiengesellschaft (BMW AG), 2019
+
+Author:
+  Sebastian Duda <sebastian.duda@fau.de>
+
+This work is licensed under the terms of the GNU GPL, version 2. See
+the COPYING file in the top-level directory.
+"""
+
+def evaluate_patches(config, prog, argv):
+    pass
