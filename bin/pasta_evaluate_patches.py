9934b9e1398a8fd3a16a1f1c8f22d9ff7b558122
Ralf Ramsauer <ralf.ramsauer@oth-regensburg.de>
2019-09-07 23:39:26 +0200

    evaluate_patches: Add R script for evaluation of characteristics.csv
    Add the evaluation script.
    
    Signed-off-by: Ralf Ramsauer <ralf.ramsauer@oth-regensburg.de>

--- bin/pasta_evaluate_patches.py
+++ bin/pasta_evaluate_patches.py
@@ -19,6 +19,8 @@ import re
 
 from logging import getLogger
 from multiprocessing import Pool, cpu_count
+from subprocess import call
+
 from tqdm import tqdm
 
 from pypasta.LinuxMaintainers import LinuxMaintainers
@@ -314,3 +316,5 @@ def evaluate_patches(config, prog, argv):
 
     dump_characteristics(characteristics, ignored_patches_related, relevant,
                          config.f_characteristics)
+
+    call(['./R/evaluate_patches.R', config.d_rout, config.f_characteristics])
