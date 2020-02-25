lfd/PaStA

commit 3b40d38da54efed8162de78fc08beb3f63b0085a

R: Rename evaluate_patches to ignored_patches
And add subsystem support.

Signed-off-by: Ralf Ramsauer <ralf.ramsauer@oth-regensburg.de>

R/ignored_patches.R
@@ -5,7 +5,7 @@
 # Copyright (c) OTH Regensburg, 2019
 #
 # Author:
-#   Ralf Ramsauer <ralf.ramsauer@othr.de>
+#   Ralf Ramsauer <ralf.ramsauer@oth-regensburg.de>
 #
 # This work is licensed under the terms of the GNU GPL, version 2.  See
 # the COPYING file in the top-level directory.
@@ -30,10 +30,10 @@ dir.create(d_dst, showWarnings = FALSE)
 
 load_csv <- function(filename) {
   data <- read.csv(filename, header = TRUE, sep=",")
-  data$upstream <- as.logical(data$upstream)
+  data$list_matches_patch <- as.logical(data$list_matches_patch)
   data$ignored <- as.logical(data$ignored)
-  data$mtrs_correct <- as.logical(data$mtrs_correct)
   data <- data %>% mutate(date = as.Date(time))
+
   # Add week info
   data <- data %>% mutate(week = as.Date(cut(date, breaks = "week")))
 
@@ -49,7 +49,7 @@ filtered_data <- raw_data
 # Filter strong outliers
 filtered_data <- filtered_data %>% filter(from != 'baolex.ni@intel.com')
 
-filtered_data <- filtered_data %>% 
+filtered_data <- filtered_data %>%
   filter(week > '2011-05-10')
 
 fname <- function(file, extension) {
@@ -84,7 +84,7 @@ ignore_rate_by_years <- function(data) {
 
   while (date_begin < date_end) {
     date_next = yearpp(date_begin)
-  
+
     relevant <- data %>% filter(date >= date_begin & date < date_next)
     cat('Ignored rate', year(date_begin) ,': ', calc_ign_rate(relevant), '\n')
 
@@ -96,44 +96,49 @@ ignored_by_week <- function(data) {
   variable <- 'ignored'
   true_case <- 'ignored'
   false_case <- 'not_ignored'
-  
-  relevant <- data %>% select(week, ignored)
-  
+
+  relevant <- data %>% select(week, ignored, list)
+
   count_predicate <- function(data, row, value, name) {
     ret <- relevant %>% filter(UQ(as.name(row)) == value)
-    ret <- ddply(ret, .(week), nrow)
-    colnames(ret) <- c('week', name)
+    ret <- ddply(ret, .(week, list), nrow)
+    colnames(ret) <- c('week', 'list', name)
     return(ret)
   }
-  
+
   true <- count_predicate(relevant, variable, TRUE, true_case)
   false <- count_predicate(relevant, variable, FALSE, false_case)
-  
-  total <- ddply(relevant, .(week), nrow)
-  colnames(total) <- c('week', 'total')
-  
-  df <- merge(x = true, y = false, by = c('week'))
-  df <- merge(x = df, y = total, by = c('week'))
-  
+
+  total <- ddply(relevant, .(week, list), nrow)
+  colnames(total) <- c('week', 'list', 'total')
+
+  # We must also merge weeks with no ignored patches, so all.x/y = TRUE
+  df <- merge(x = true, y = false, by = c('week', 'list'), all.x = TRUE, all.y = TRUE)
+  df <- merge(x = df, y = total, by = c('week', 'list'), all.x = TRUE, all.y = TRUE)
+  # Then, replace NA by 0
+  df[is.na(df)] <- 0
+
   df$fraction <- df$ignored / df$total
-  
-  df <- melt(df, id.vars = c('week'))
-  
-    relevant <- df %>% filter(variable == true_case | variable == 'total')
+
+  df <- melt(df, id.vars = c('week', 'list'))
+
+  relevant <- df %>% filter(variable == true_case | variable == 'total')
   plot <- ggplot(relevant,
                  aes(x = week, y = value, color = variable)) +
     geom_line() +
-    geom_smooth(method = "lm") +
-    scale_y_log10() +
-    ylab('Number of patches') +
+    geom_smooth() +
+    #scale_y_sqrt(breaks = c(20, 60, 100, 150, 700, 1000, 2000, 3000, 4000)) +
+    scale_y_sqrt(breaks = c(10, 50, 100, 250, 500, 1000)) +
+    ylab('Number of patches per week') +
     xlab('Date') +
     scale_x_date(date_breaks = '1 year', date_labels = '%Y') +
     theme_bw(base_size = 15) +
-    # theme(legend.position = 'top') +
-    labs(color = '')
-  printplot(plot, 'ignored_by_week_total', 3)
-  
-  
+    theme(legend.position = 'none') +
+    labs(color = '') +
+    facet_wrap(~list, scales = 'free')
+  printplot(plot, 'ignored_by_week_total', 4.5)
+
+
   relevant <- df %>% filter(variable == 'ignored') #%>% select(week, value)
   plot <- ggplot(relevant,
                  aes(x = week, y = value, color = variable)) +
@@ -143,11 +148,11 @@ ignored_by_week <- function(data) {
     theme_bw(base_size = 15) +
     scale_x_date(date_breaks = '1 year', date_labels = '%Y') +
     xlab('Date') +
-    ylab('Total number of ignored patches') +
-    ylim(c(0, 150)) +
-    theme(legend.position = 'None')
-  printplot(plot, 'ignored_by_week_ignored_only', 3)
-                
+    ylab('Total number of ignored patches per week') +
+    theme(legend.position = 'None') +
+    facet_wrap(~list, scales = 'free')
+  printplot(plot, 'ignored_by_week_ignored_only', 4.5)
+
   relevant <- df %>% filter(variable == 'fraction')
   plot <- ggplot(relevant,
                  aes(x = week, y = value, color = variable)) +
@@ -156,31 +161,33 @@ ignored_by_week <- function(data) {
     geom_smooth(fill = 'blue', colour = 'black') +
     theme_bw(base_size = 15) +
     scale_x_date(date_breaks = '1 year', date_labels = '%Y') +
-    scale_y_continuous(labels = scales::percent_format(accuracy = 1, suffix = "\\%"),
-                       breaks = seq(0.01, 0.06, by = 0.01)) +
+    #scale_y_continuous(labels = scales::percent_format(accuracy = 1, suffix = "\\%"),
+    #                   breaks = seq(0.01, 0.06, by = 0.01)) +
+    scale_y_continuous(labels = scales::percent_format(accuracy = 1, suffix = "\\%")) +
     xlab('Date') +
     ylab('Ratio of ignored patches') +
     #ylab('Ratio of correctly addressed maintainers') +
-    theme(legend.position = 'None')
-  printplot(plot, 'ignored_by_week_fraction', 3)
+    theme(legend.position = 'None') +
+    facet_wrap(~list, scales = 'free')
+      printplot(plot, 'ignored_by_week_fraction', 4.5)
 }
 
 ignored_by_rc <- function(data) {
-  data <- data %>% select('kv', 'rc', 'ignored')
-  
-  total <- ddply(data, .(kv, rc), nrow)
-  colnames(total) <- c('kv', 'rc', 'total')
-  
+  data <- data %>% select('list', 'kv', 'rc', 'ignored')
+
+  total <- ddply(data, .(list, kv, rc), nrow)
+  colnames(total) <- c('list', 'kv', 'rc', 'total')
+
   ignored <- data %>% filter(ignored == TRUE)
-  ignored <- ddply(ignored, .(kv, rc), nrow)
-  colnames(ignored) <- c('kv', 'rc', 'ignored')
-  
-  df <- merge(x = total, y = ignored, by = c('kv', 'rc'))
+  ignored <- ddply(ignored, .(list, kv, rc), nrow)
+  colnames(ignored) <- c('list', 'kv', 'rc', 'ignored')
+
+  df <- merge(x = total, y = ignored, by = c('list', 'kv', 'rc'))
   df$fraction <- df$ignored / df$total
-  df <- melt(df, id.vars = c('kv', 'rc'))
-  
-  relevant <- df %>% filter(variable == 'fraction') %>% select(kv, rc, value)
-  
+  df <- melt(df, id.vars = c('list', 'kv', 'rc'))
+
+  relevant <- df %>% filter(variable == 'fraction') %>% select(list, kv, rc, value)
+
   plot <- ggplot(relevant,
                  aes(x = rc, y = value, group = rc)) +
     geom_boxplot() +
@@ -190,27 +197,28 @@ ignored_by_rc <- function(data) {
     scale_y_continuous(labels = scales::percent_format(accuracy = 1, suffix = "\\%"),
                        breaks = seq(0.01, 0.06, by = 0.01)) +
     xlab('Development Stage (-rc)') +
-    ylab('Probability that patch is ignored')
-    printplot(plot, 'ignored_by_rc', 0)
+    ylab('Probability that patch is ignored') +
+    facet_wrap(~list)
+  printplot(plot, 'ignored_by_rc', 0)
 }
 
   scatterplots <- function(data) {
   data <- filtered_data
-  
+
   ignored <- data %>% filter(ignored == TRUE) %>% select(from) %>% count
   colnames(ignored) <- c('from', 'ignored')
-  
+
   not_ignored <- data %>% filter(ignored == FALSE) %>% select(from) %>% count
   colnames(not_ignored) <- c('from', 'not_ignored')
-  
+
   total <- data %>% select(from) %>% count
   colnames(total) <- c('from', 'total')
-  
+
   df <- merge(x = ignored, y = not_ignored, by = c('from'))
   df <- merge(x = df, y = total, by = c('from'))
-  
+
   df$ratio <- df$ignored / df$total
-  
+
   relevant <- df %>% filter(total < 4000) %>% filter(ignored < 400)
   # relevant <- df
   plot <- ggplot(relevant, aes(x = total, y = ignored)) +
@@ -219,31 +227,31 @@ ignored_by_rc <- function(data) {
     ylab('Number of ignored patches') +
     theme_bw(base_size = 15)
   printplot(plot, 'foo5', 2)
-  
+
   relevant <- df %>% filter(total < 101)
   plot <- ggplot(relevant, aes(x = total, y = ignored)) +
     geom_point() + geom_density2d()
   printplot(plot, 'foo6', 0)
-  
+
     relevant <- df %>% filter(total < 101)
   plot <- ggplot(relevant, aes(x = total, y = ratio)) +
     geom_point() + geom_density2d()
   printplot(plot, 'foo7', 0)
 }
-  
+
 week_scatterplots <- function(data) {
   data <- filtered_data
-  
+
   data <- data %>% select(week, ignored)
-  
+
   total <- ddply(data, .(week), nrow)
   colnames(total) <- c('week', 'total')
-  
+
   ignored <- ddply(data %>% filter(ignored == TRUE), .(week), nrow)
   colnames(ignored) <- c('week', 'ignored')
-  
+
   df = merge(x = total, y = ignored, by = c('week'))
-  
+
   plot <- ggplot(df, aes(x = total, y = ignored)) +
     geom_point() +
     theme_bw(base_size = 15) +
@@ -252,13 +260,28 @@ week_scatterplots <- function(data) {
   printplot(plot, 'ignored_week_scatter', 0)
 }
 
-ignore_rate_by_years(filtered_data)
-#ignored_by_rc(raw_data)
-ignored_by_rc(filtered_data)
+selection <- filtered_data %>%
+  filter(list %in% c('linux-arm-kernel@lists.infradead.org',
+                     'netdev@vger.kernel.org',
+                     #'linux-wireless@vger.kernel.org',
+                     
+                     'linuxppc-dev@lists.ozlabs.org',
+                     'linux-mips@vger.kernel.org'
+                     #'linux-pci@vger.kernel.org'
+                     ))
 
-#filtered_data$ignored <- filtered_data$mtrs_correct
+all <- filtered_data
+all$list <- 'Overall'
+all <- all %>% filter(week < '2018-12-24') %>% distinct
+
+#ignore_rate_by_years(all)
+
+#ignored_by_week(selection)
+#ignored_by_week(all)
+
+#ignored_by_rc(selection)
+ignored_by_rc(all)
+
 #filtered_data <- filtered_data %>% filter(kv != 'v2.6.39')
-ignored_by_week(filtered_data)
-
-#scatterplots(filtered_data)
-#week_scatterplots(filtered_data)
+#scatterplots(all)
+#week_scatterplots(all)

bin/pasta_evaluate_patches.py
@@ -330,4 +330,4 @@ def evaluate_patches(config, prog, argv):
     dump_characteristics(repo, characteristics, ignored_patches_related,
                          relevant, config.f_characteristics)
 
-    call(['./R/evaluate_patches.R', config.d_rout, config.f_characteristics])
+    call(['./R/ignored_patches.R', config.d_rout, config.f_characteristics])

commit 8b708e0af55e4170357022a2650b367a8cefc7b0

R: ignored_patches: Fully respect weeks with no observations
There are weeks, where no single patch was received on a mailing list.
In these cases, we have no observations. This leads to inconsistencies
when graphs are plot with geom_line().

Fix this by filling up those weeks with zeroes.

Fixes: 3b40d38da54 ("R: Rename evaluate_patches to ignored_patches")
Reported-by: Sebastian Duda <sebastian.duda@fau.de>
Signed-off-by: Ralf Ramsauer <ralf.ramsauer@oth-regensburg.de>
R-Magic-by: Wolfgang Mauerer <wolfgang.mauerer@oth-regensburg.de>

R/ignored_patches.R
@@ -112,6 +112,24 @@ ignored_by_week <- function(data) {
   total <- ddply(relevant, .(week, list), nrow)
   colnames(total) <- c('week', 'list', 'total')
 
+  fillup_missing_weeks <- function(data, key) {
+    min.week <- min(data$week)
+    delta.weeks <- days(max(data$week) - min.week)$day / 7
+
+    all.weeks <- as.Date(sapply(0:delta.weeks, function(n) {
+      return (as.character(min.week + weeks(n)))
+    }))
+
+    missing.weeks <- !(all.weeks %in% data$week)
+    if (any(missing.weeks)) {
+      return(rbind(data, data.frame(week=all.weeks[missing.weeks], total=0)))
+    }
+    return (data)
+  }
+
+  # Fill up weeks with no values with zeroes
+  total <- total %>% group_by(list) %>% group_modify(fillup_missing_weeks)
+
   # We must also merge weeks with no ignored patches, so all.x/y = TRUE
   df <- merge(x = true, y = false, by = c('week', 'list'), all.x = TRUE, all.y = TRUE)
   df <- merge(x = df, y = total, by = c('week', 'list'), all.x = TRUE, all.y = TRUE)