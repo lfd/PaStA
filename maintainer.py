f98159b11fe455363b8d571510b46870ec4622e6
Basak <basak@pop-os.localdomain>
2020-03-14 17:25:43 +0100

    example usage

--- maintainer.py
+++ maintainer.py
@@ -5,21 +5,15 @@ from pypasta.LinuxMaintainers import LinuxMaintainers
 from sys import argv
 
 def get_maintainers(filenames):
-    print(filenames)
+    results = list()
     maintainers = open('./resources/linux/repo/MAINTAINERS', 'r').read()
     maintain = LinuxMaintainers(maintainers)
+
     for filename in filenames:
         subsystem = maintain.get_subsystems_by_file(filename)
-        maintainer = maintain.get_maintainers(subsystem)
-        return(filename, subsystem, maintainer)
-
-print('Number of arguments:', len(argv), 'arguments.')
-print( 'Argument List:', str(argv))
+        maintainer = maintain.get_maintainers(subsystem.pop())
+        results.append((filename, subsystem, maintainer))
+    return results
+#(PaStA) basak@pop-os:~/Documents/PaStA$ python ./maintainer.py repo/drivers/acpi/acpica/accommon.
 argv.pop(0)
-print(type(argv))
-filenames = list(argv)
-print(type(filenames))
-print(get_maintainers(filenames))
-
-
-
+print(get_maintainers(argv))
