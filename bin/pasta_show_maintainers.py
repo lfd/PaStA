ef8dd57f69159b476c248f28b86a342ac0fff298
Başak Erdamar <20912429+basakerdamar@users.noreply.github.com>
2020-03-17 11:13:51 +0100

    Added MAINTAINERS file option

--- bin/pasta_show_maintainers.py
+++ bin/pasta_show_maintainers.py
@@ -1,11 +1,10 @@
 """
-
 This work is licensed under the terms of the GNU GPL, version 2.  See
 the COPYING file in the top-level directory.
-
 """
 
 # ./pasta maintainers drivers/acpi/acpica/accommon.h sound/atmel/ac97c.c
+# ./pasta maintainers --file ./resources/linux/repo/MAINTAINERS  drivers/acpi/acpica/accommon.h sound/atmel/ac97c.c
 
 import sys
 import os
@@ -20,13 +19,18 @@ log = getLogger(__name__[-15:])
 
 def get_maintainers(config, sub, argv):
 
-    results = dict()
-    all_maintainers = open('./resources/linux/repo/MAINTAINERS', 'r').read()
-    all_maintainers = LinuxMaintainers(all_maintainers)
+    if argv[0] == '--file':
+        argv.pop(0)
+        all_maintainers = open(argv.pop(0), 'r').read()
+    else:
+        all_maintainers = open('./resources/linux/repo/MAINTAINERS', 'r').read()
 
+    all_maintainers = LinuxMaintainers(all_maintainers)
+    
+    results = dict()
     for filename in argv:
         subsystem = all_maintainers.get_subsystems_by_file(filename)
         maintainer = all_maintainers.get_maintainers(subsystem.pop())
         results[filename] = maintainer
+    
     return results
-
