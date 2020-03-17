55cd035357fa10ffd30b3cbfa4cfcc1bd4e6bc17
Başak Erdamar <20912429+basakerdamar@users.noreply.github.com>
2020-03-17 10:24:36 +0100

    Prettier results

--- bin/pasta_show_maintainers.py
+++ bin/pasta_show_maintainers.py
@@ -2,9 +2,10 @@
 
 This work is licensed under the terms of the GNU GPL, version 2.  See
 the COPYING file in the top-level directory.
+
 """
 
-# python  ./bin/pasta_show_maintainers.py drivers/acpi/acpica/accommon.h sound/atmel/ac97c.c
+# ./pasta maintainers drivers/acpi/acpica/accommon.h sound/atmel/ac97c.c
 
 import sys
 import os
@@ -18,15 +19,14 @@ from pypasta.LinuxMaintainers import LinuxMaintainers
 log = getLogger(__name__[-15:])
 
 def get_maintainers(config, sub, argv):
-    results = list()
-    maintainers = open('./resources/linux/repo/MAINTAINERS', 'r').read()
-    maintain = LinuxMaintainers(maintainers)
+
+    results = dict()
+    all_maintainers = open('./resources/linux/repo/MAINTAINERS', 'r').read()
+    all_maintainers = LinuxMaintainers(all_maintainers)
 
     for filename in argv:
-        subsystem = maintain.get_subsystems_by_file(filename)
-        maintainer = maintain.get_maintainers(subsystem.pop())
-        results.append((filename, subsystem, maintainer))
+        subsystem = all_maintainers.get_subsystems_by_file(filename)
+        maintainer = all_maintainers.get_maintainers(subsystem.pop())
+        results[filename] = maintainer
     return results
 
-#sys.argv.pop(0)
-#print(*get_maintainers(sys.argv), sep='\n')
