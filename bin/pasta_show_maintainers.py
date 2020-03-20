d9927a1cd470d1efe8738cc6b15aabd494e9e73b
Başak Erdamar <basakerdamar@gmail.com>
2020-03-20 10:42:07 +0100

    LoC

--- bin/pasta_show_maintainers.py
+++ bin/pasta_show_maintainers.py
@@ -22,27 +22,37 @@ def flatten(nested):
             flat_list.append(item)
     return flat_list
 
+def file_len(filename):
+    with open(filename) as f:
+        for i, l in enumerate(f):
+            pass
+    return i + 1
+
 def get_maintainers(config, sub, argv):
-        
+    print(argv.pop(0))
     if argv.pop(0) == '--filter':
         filenames = file_to_string(argv.pop(0)).splitlines()
     else:
-        print('usage: ./pasta maintainers --filter <filelist text file> [--file <MAINTAINERS file>]')
+        print('usage: ./pasta maintainers show entries --filter <filelist text file> [--file <MAINTAINERS file>]')
         exit()
-        
+    
+    results = list()
+
     if argv[0] == '--file':
         argv.pop(0)
         all_maintainers_text = file_to_string(argv.pop(0))
     else:
         all_maintainers_text = file_to_string('./resources/linux/repo/MAINTAINERS')
+    query = argv.pop(0)
+    if query == 'entries':
+        for filename in filenames:
+            results.append(filename + str(file_len(filename)))
+        return results
+    elif query == "maitainers":
+        all_maintainers = LinuxMaintainers(all_maintainers_text)
+        subsystems = all_maintainers.get_subsystems_by_files(filenames)
 
-    all_maintainers = LinuxMaintainers(all_maintainers_text)
-
-    subsystems = all_maintainers.get_subsystems_by_files(filenames)
-
-    results = list()
-    for subsystem in subsystems:
-        maintainer = all_maintainers.get_maintainers(subsystem)
-        results.append(maintainer)
-
-    return set(flatten(flatten(results)))
+        for subsystem in subsystems:
+            maintainer = all_maintainers.get_maintainers(subsystem)
+            results.append(maintainer)
+        return set(flatten(flatten(results)))
