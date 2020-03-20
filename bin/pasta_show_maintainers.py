25605fc6f97339052529f36e0482de3322128dae
Başak Erdamar <basakerdamar@gmail.com>
2020-03-20 21:55:00 +0100

    Command options logic

--- bin/pasta_show_maintainers.py
+++ bin/pasta_show_maintainers.py
@@ -29,30 +29,39 @@ def file_len(filename):
     return i + 1
 
 def get_maintainers(config, sub, argv):
-    print(argv.pop(0))
-    if argv.pop(0) == '--filter':
-        filenames = file_to_string(argv.pop(0)).splitlines()
+
+    if argv.pop(-2) == '--file':
+        all_maintainers_text = file_to_string(argv.pop(-1))
+    else:
+        all_maintainers_text = file_to_string('./resources/linux/repo/MAINTAINERS')
+    
+    results = list()
+    argv.pop(0)
+    query = argv.pop(0)
+    print("query", query)
+    if query == 'entries':
+        if len(argv)==0:
+            #./pasta maintainers show entries
+            pass
+        elif argv.pop(0) == '--filter':
+            filenames = file_to_string(argv.pop(0)).splitlines()
+            for filename in filenames:
+                results.append(filename + str(file_len(filename)))
+        return results
+    elif query == "maintainers":
+        if len(argv)==0:
+            #./pasta maintainers show maintainers
+            pass
+        elif argv.pop(0) == '--filter':
+            all_maintainers = LinuxMaintainers(all_maintainers_text)
+            subsystems = all_maintainers.get_subsystems_by_files(filenames)
+
+            for subsystem in subsystems:
+                maintainer = all_maintainers.get_maintainers(subsystem)
+                results.append(maintainer)
+        print(results)
+        return set(flatten(flatten(results))) 
     else:
         print('usage: ./pasta maintainers show entries --filter <filelist text file> [--file <MAINTAINERS file>]')
         exit()
     
-    results = list()
-
-    if argv[0] == '--file':
-        argv.pop(0)
-        all_maintainers_text = file_to_string(argv.pop(0))
-    else:
-        all_maintainers_text = file_to_string('./resources/linux/repo/MAINTAINERS')
-    query = argv.pop(0)
-    if query == 'entries':
-        for filename in filenames:
-            results.append(filename + str(file_len(filename)))
-        return results
-    elif query == "maitainers":
-        all_maintainers = LinuxMaintainers(all_maintainers_text)
-        subsystems = all_maintainers.get_subsystems_by_files(filenames)
-
-        for subsystem in subsystems:
-            maintainer = all_maintainers.get_maintainers(subsystem)
-            results.append(maintainer)
-        return set(flatten(flatten(results)))
