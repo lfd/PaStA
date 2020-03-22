ef977675c0580e48c66b43d0e64960a174986895
Basak <basak@pop-os.localdomain>
2020-03-22 18:05:22 +0100

    Pretty results and meaningful return values

--- bin/pasta_show_maintainers.py
+++ bin/pasta_show_maintainers.py
@@ -8,7 +8,7 @@ import os
 
 from logging import getLogger
 
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from pypasta.LinuxMaintainers import LinuxMaintainers
 from pypasta.Util import file_to_string
@@ -23,45 +23,64 @@ def flatten(nested):
     return flat_list
 
 def file_len(filename):
-    with open(filename) as f:
-        for i, l in enumerate(f):
-            pass
+    f = file_to_string(filename)
+    for i, l in enumerate(f):
+        pass
     return i + 1
 
 def get_maintainers(config, sub, argv):
 
-    if argv.pop(-2) == '--file':
-        all_maintainers_text = file_to_string(argv.pop(-1))
+    if '--file' in argv:
+        index = argv.index('--file')
+        argv.pop(index)
+        all_maintainers_text = file_to_string(argv.pop(index))
     else:
         all_maintainers_text = file_to_string('./resources/linux/repo/MAINTAINERS')
     
-    results = list()
+    all_kernel_files = []
+    filenames = []
+    total_loc= 0
+    
+    for r, d, f in os.walk('./resources/linux/repo/kernel/'):
+        for item in f:
+            filename = os.path.join(r, item)
+            total_loc += file_len(filename)
+            all_kernel_files.append(filename)
+
+    if '--filter' in argv:
+        index = argv.index('--filter')
+        argv.pop(index)
+        filenames = file_to_string(argv.pop(index)).splitlines()
+    else:
+        filenames = all_kernel_files
+
     argv.pop(0)
     query = argv.pop(0)
-    print("query", query)
-    if query == 'entries':
-        if len(argv)==0:
-            #./pasta maintainers show entries
-            pass
-        elif argv.pop(0) == '--filter':
-            filenames = file_to_string(argv.pop(0)).splitlines()
-            for filename in filenames:
-                results.append(filename + str(file_len(filename)))
-        return results
-    elif query == "maintainers":
-        if len(argv)==0:
-            #./pasta maintainers show maintainers
-            pass
-        elif argv.pop(0) == '--filter':
-            all_maintainers = LinuxMaintainers(all_maintainers_text)
-            subsystems = all_maintainers.get_subsystems_by_files(filenames)
 
+    if query == 'entries':
+
+        print('File name\tLines of code\tLines of code / Total lines of code')
+        for filename in filenames:
+            loc = file_len(str.strip(filename))
+            print(filename + '\t' + str(loc) + '\t' + str(loc/total_loc))
+
+    elif query == "maintainers":
+
+        all_maintainers = LinuxMaintainers(all_maintainers_text)
+        maintainers = []
+        print('Maintainers of:')
+        for filename in filenames:
+            subsystems = all_maintainers.get_subsystems_by_file(filename)
             for subsystem in subsystems:
-                maintainer = all_maintainers.get_maintainers(subsystem)
-                results.append(maintainer)
-        print(results)
-        return set(flatten(flatten(results))) 
+                maintainers.append(all_maintainers.get_maintainers(subsystem))
+
+            print('\n' + filename + ':')
+            for entry in set(flatten(flatten(maintainers))):
+                if type(entry) is str:
+                    print('\t', entry)
+                else:
+                    print('\t', entry[0], entry[1])
+        return 0
     else:
-        print('usage: ./pasta maintainers show entries --filter <filelist text file> [--file <MAINTAINERS file>]')
-        exit()
-    
+        print('usage: ./pasta maintainers show entries [--filter <filelist text file>] [--file <MAINTAINERS file>]')
+        return -1
