ca291669189ec4ae4fcc2b90b7dc05711c581a7a
Anmol Singh <anmol.singh@bmw.de>
2020-06-04 15:48:32 +0200

    Refactor pasta_evaluate_patches to generalize data preparation
    Prepare data from pasta result for several analyses selectively.
    pasta evaluate_patches is generalized to support specific data preparation
    steps based on options given. Ignored patch code is refactored in the new
    structure. Current options: -- ignored, --off-list, and --review.
    Off-list and review options are to be implemented in the placeholders.
    The resulting data dumps would serve as input to respective R/python
    scripts for further analysis.
    
    Signed-off-by: Anmol Singh <anmol.singh@bmw.de>
    Signed-off-by: Ralf Ramsauer <ralf.ramsauer@oth-regensburg.de>

--- bin/pasta_evaluate_patches.py
+++ bin/pasta_evaluate_patches.py
@@ -12,6 +12,7 @@ This work is licensed under the terms of the GNU GPL, version 2. See
 the COPYING file in the top-level directory.
 """
 
+import argparse
 import csv
 import re
 
@@ -114,33 +115,6 @@ def get_relevant_patches(characteristics):
     return relevant
 
 
-def get_ignored(characteristics, clustering, relevant):
-    # Calculate ignored patches
-    ignored_patches = {patch for patch in relevant if
-                       not characteristics[patch].is_upstream and
-                       not characteristics[patch].has_foreign_response}
-
-    # Calculate ignored patches wrt to other patches in the cluster: A patch is
-    # considered as ignored, if all related patches were ignoreed as well
-    ignored_patches_related = \
-        {patch for patch in ignored_patches if False not in
-         [characteristics[x].has_foreign_response == False
-          for x in (clustering.get_downstream(patch) & relevant)]}
-
-    num_relevant = len(relevant)
-    num_ignored_patches = len(ignored_patches)
-    num_ignored_patches_related = len(ignored_patches_related)
-
-    log.info('Found %u ignored patches' % num_ignored_patches)
-    log.info('Fraction of ignored patches: %0.3f' %
-             (num_ignored_patches / num_relevant))
-    log.info('Found %u ignored patches (related)' % num_ignored_patches_related)
-    log.info('Fraction of ignored related patches: %0.3f' %
-            (num_ignored_patches_related / num_relevant))
-
-    return ignored_patches, ignored_patches_related
-
-
 def check_correct_maintainer_patch(c):
     # Metric: All lists + at least one maintainer per subsystem
     # needs to be addressed correctly
@@ -172,85 +146,228 @@ def check_correct_maintainer_patch(c):
     return False
 
 
-def get_kv_rc(linux_version):
-    tag = linux_version.split('-rc')
-    kv = tag[0]
-    rc = 0
-    if len(tag) == 2:
-        rc = int(tag[1])
+def load_maintainers(tag):
+    pyrepo = _repo.repo
 
-    return kv, rc
+    tag_hash = pyrepo.lookup_reference('refs/tags/%s' % tag).target
+    commit_hash = pyrepo[tag_hash].target
+    maintainers_blob_hash = pyrepo[commit_hash].tree['MAINTAINERS'].id
+    maintainers = pyrepo[maintainers_blob_hash].data
+
+    try:
+        maintainers = maintainers.decode('utf-8')
+    except:
+        # older versions use ISO8859
+        maintainers = maintainers.decode('iso8859')
+
+    m = LinuxMaintainers(maintainers)
+
+    return tag, m
 
 
-def dump_characteristics(repo, characteristics, ignored, relevant, filename):
-    with open(filename, 'w') as csv_file:
-        csv_fields = ['id', 'from', 'list', 'list_matches_patch', 'kv', 'rc',
-                      'ignored', 'time']
-        writer = csv.DictWriter(csv_file, fieldnames=csv_fields)
-        writer.writeheader()
+def load_all_maintainers(ret, repo):
+    if ret is None:
+        ret = dict()
 
-        for message_id in sorted(relevant):
-            c = characteristics[message_id]
-            kv, rc = get_kv_rc(c.linux_version)
-            mail_from = c.mail_from[1]
+    tags = {x[0] for x in repo.tags if not x[0].startswith('v2.6')}
+    tags |= {x[0] for x in repo.tags if x[0].startswith('v2.6.39')}
 
-            for list in repo.mbox.get_lists(message_id):
-                list_matches_patch = False
-                for subsys in c.maintainers.values():
-                    lists = subsys[0]
-                    if list in lists:
-                        list_matches_patch = True
-                        break
+    # Only load what's not yet cached
+    tags -= ret.keys()
 
-                row = {'id': message_id,
-                       'from': mail_from,
-                       'list': list,
-                       'list_matches_patch': list_matches_patch,
-                       'kv': kv,
-                       'rc': rc,
-                       'ignored': message_id in ignored,
-                       'time': c.date,
-                }
+    if len(tags) == 0:
+        return ret, False
 
-                writer.writerow(row)
+
+    global _repo
+    _repo = repo
+    p = Pool(processes=cpu_count())
+    for tag, maintainers in tqdm(p.imap_unordered(load_maintainers, tags),
+                                 total=len(tags), desc='MAINTAINERS'):
+        ret[tag] = maintainers
+    p.close()
+    p.join()
+    _repo = None
+
+    return ret, True
+
+
+def prepare_ignored_patches(config, repo, clustering):
+    repo.mbox.load_threads()
+
+    all_messages_in_time_window = repo.mbox.get_ids(config.mbox_time_window,
+                                                    allow_invalid=True)
+
+    def _get_ignored(characteristics, clustering, relevant):
+        # Calculate ignored patches
+        ignored_patches = {patch for patch in relevant if
+                           not characteristics[patch].is_upstream and
+                           not characteristics[patch].has_foreign_response}
+
+        # Calculate ignored patches wrt to other patches in the cluster: A patch is
+        # considered as ignored, if all related patches were ignoreed as well
+        ignored_patches_related = \
+            {patch for patch in ignored_patches if False not in
+             [characteristics[x].has_foreign_response == False
+              for x in (clustering.get_downstream(patch) & relevant)]}
+
+        num_relevant = len(relevant)
+        num_ignored_patches = len(ignored_patches)
+        num_ignored_patches_related = len(ignored_patches_related)
+
+        log.info('Found %u ignored patches' % num_ignored_patches)
+        log.info('Fraction of ignored patches: %0.3f' %
+                 (num_ignored_patches / num_relevant))
+        log.info('Found %u ignored patches (related)' % num_ignored_patches_related)
+        log.info('Fraction of ignored related patches: %0.3f' %
+                 (num_ignored_patches_related / num_relevant))
+
+        return ignored_patches, ignored_patches_related
+
+    def _load_characteristics(ret, repo):
+        if ret is None:
+            ret = dict()
+
+        missing = all_messages_in_time_window - ret.keys()
+        if len(missing) == 0:
+            return ret, False
+
+        missing = load_linux_mail_characteristics(repo,
+                                                  missing,
+                                                  maintainers_version,
+                                                  clustering)
+
+        return {**ret, **missing}, True
+
+    def _load_pkl_and_update(filename, update_command, repo):
+        ret = None
+        if os.path.isfile(filename):
+            ret = pickle.load(open(filename, 'rb'))
+
+        ret, changed = update_command(ret, repo)
+        if changed:
+            pickle.dump(ret, open(filename, 'wb'))
+
+        return ret
+
+    def _get_kv_rc(linux_version):
+        tag = linux_version.split('-rc')
+        kv = tag[0]
+        rc = 0
+        if len(tag) == 2:
+            rc = int(tag[1])
+
+        return kv, rc
+
+    def _dump_characteristics(repo, characteristics, ignored, relevant, filename):
+        with open(filename, 'w') as csv_file:
+            csv_fields = ['id', 'from', 'list', 'list_matches_patch', 'kv', 'rc',
+                          'ignored', 'time']
+            writer = csv.DictWriter(csv_file, fieldnames=csv_fields)
+            writer.writeheader()
+
+            for message_id in sorted(relevant):
+                c = characteristics[message_id]
+                kv, rc = _get_kv_rc(c.linux_version)
+                mail_from = c.mail_from[1]
+
+                for list in repo.mbox.get_lists(message_id):
+                    list_matches_patch = False
+                    for subsys in c.maintainers.values():
+                        lists = subsys[0]
+                        if list in lists:
+                            list_matches_patch = True
+                            break
+
+                    row = {'id': message_id,
+                           'from': mail_from,
+                           'list': list,
+                           'list_matches_patch': list_matches_patch,
+                           'kv': kv,
+                           'rc': rc,
+                           'ignored': message_id in ignored,
+                           'time': c.date,
+                           }
+
+                    writer.writerow(row)
+
+    log.info('Loading/Updating MAINTAINERS...')
+    maintainers_version = _load_pkl_and_update(config.f_maintainers_pkl,
+                                               load_all_maintainers, repo)
+
+    log.info('Loading/Updating Linux patch characteristics...')
+    characteristics = _load_pkl_and_update(config.f_characteristics_pkl,
+                                           _load_characteristics, repo)
+
+    relevant = get_relevant_patches(characteristics)
+
+    log.info('Identify ignored patches...')
+    ignored_patches, ignored_patches_related = _get_ignored(characteristics,
+                                                            clustering,
+                                                            relevant)
+
+    _dump_characteristics(repo, characteristics, ignored_patches_related,
+                          relevant, config.f_characteristics)
+
+    call(['./R/ignored_patches.R', config.d_rout, config.f_characteristics])
+
+
+def prepare_off_list_patches():
+    pass
+
+
+def prepare_patch_review():
+    pass
 
 
 def evaluate_patches(config, prog, argv):
+    parser = argparse.ArgumentParser(prog=prog,
+                                     description='aggregate commit and patch info')
+
+    parser.add_argument('--ignored',
+                        action='store_const',
+                        const='ignored',
+                        dest='mode',
+                        help='prepare data for patch analysis \n'
+                             'prepare data for ignored patch analysis \n'
+                        )
+
+    parser.add_argument('--off-list',
+                        action='store_const',
+                        const='off-list',
+                        dest='mode',
+                        help='prepare data for off-list patch analysis \n')
+
+    parser.add_argument('--review',
+                        action='store_const',
+                        const='review',
+                        dest='mode',
+                        help='prepare data for patch review analysis \n')
+
+    analysis_option = parser.parse_args(argv)
+
+    if not analysis_option.mode:
+        parser.error("No action requested, one of --ignored, --off-list, or --review must be given")
+
     if config.mode != config.Mode.MBOX:
-        log.error('Only works in Mbox mode!')
+        log.error("Only works in Mbox mode!")
         return -1
+
+    if config.mbox_use_patchwork_id:
+        log.error('pasta prepare_evaluation does not work with '
+                  'USE_PATCHWORK_ID = true')
 
     repo = config.repo
     _, clustering = config.load_cluster()
     clustering.optimize()
 
     config.load_ccache_mbox()
-    repo.mbox.load_threads()
 
-    patches = set()
-    upstream = set()
-    for d, u in clustering.iter_split():
-        patches |= d
-        upstream |= u
+    if analysis_option.mode == 'ignored':
+        prepare_ignored_patches(config, repo, clustering)
 
-    all_messages_in_time_window = repo.mbox.get_ids(config.mbox_time_window,
-                                                    allow_invalid=True)
+    elif analysis_option.mode == 'off-list':
+        prepare_off_list_patches()
 
-    tags = {x[0] for x in repo.tags if not x[0].startswith('v2.6')}
-    tags |= {x[0] for x in repo.tags if x[0].startswith('v2.6.39')}
-    maintainers_version = load_maintainers(config, tags)
-    characteristics = \
-        load_linux_mail_characteristics(config, maintainers_version, clustering,
-                                        all_messages_in_time_window)
-
-    relevant = get_relevant_patches(characteristics)
-
-    log.info('Identify ignored patches...')
-    ignored_patches, ignored_patches_related = get_ignored(characteristics,
-                                                           clustering,
-                                                           relevant)
-
-    dump_characteristics(repo, characteristics, ignored_patches_related,
-                         relevant, config.f_characteristics)
-
-    call(['./R/ignored_patches.R', config.d_rout, config.f_characteristics])
+    else:
+        prepare_patch_review()
