lfd/PaStA

commit cf43669b49ba9f191cdfe39b48187fe91898b936

tools: process_mail_pipe.sh: remove trailing newline character
Formail will give us a second, trailing, newline character when it
splits up mbox files. Remove that newline character.

This does not apply for maildirs, so only fix it in
process_mail_pipe.sh.

Signed-off-by: Ralf Ramsauer <ralf.ramsauer@oth-regensburg.de>

tools/process_mail_pipe.sh
@@ -13,7 +13,8 @@ LISTNAME=$2
 BASEDIR=$3
 TMP=$(mktemp)
 
-cat /dev/stdin > $TMP
+# Remove last character (\n) from the input and store the file
+head -c -1 /dev/stdin > $TMP
 ./process_mail.sh $ARCHIVE_TYPE $LISTNAME $BASEDIR $TMP
 RET=$?
 

commit 15a1c9bc23fd89211062b1cb1591f70899a96484

tools: process_mail_pipe: don't chomp the last newline
This led to errors when parsing some mails from raw mboxes. Instead,
append the newline to the patches that were received via API.

Fixes: cf43669b49ba ("tools: process_mail_pipe.sh: remove trailing newline character")
Signed-off-by: Ralf Ramsauer <ralf.ramsauer@oth-regensburg.de>

pypasta/Repository/Mbox.py
@@ -431,8 +431,12 @@ class PatchworkProject(MailContainer):
     def _pull_patch(url):
         resp = requests.get(url)
         resp.raise_for_status()
-        md5sum = hashlib.md5(resp.content).hexdigest()
-        return md5sum, resp.content
+        # Add a trailing newline. This is required that the hash that is
+        # received via API has the same MD5 sum as the ones that were
+        # imported via the initial_mbox
+        content = resp.content + b'\n'
+        md5sum = hashlib.md5(content).hexdigest()
+        return md5sum, content
 
     def update(self):
         if self.f_mbox_raw:

tools/process_mail_pipe.sh
@@ -13,8 +13,7 @@ LISTNAME=$2
 BASEDIR=$3
 TMP=$(mktemp)
 
-# Remove last character (\n) from the input and store the file
-head -c -1 /dev/stdin > $TMP
+cat /dev/stdin > $TMP
 ./process_mail.sh $ARCHIVE_TYPE $LISTNAME $BASEDIR $TMP
 RET=$?
 