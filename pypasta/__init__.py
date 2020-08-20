596f2ebb21f6a610c8fabae007d0d176ba8a6164
Ralf Ramsauer <ralf.ramsauer@oth-regensburg.de>
2020-08-20 18:55:57 +0200

    pypasta: export load_maintainers
    Signed-off-by: Ralf Ramsauer <ralf.ramsauer@oth-regensburg.de>

--- pypasta/__init__.py
+++ pypasta/__init__.py
@@ -29,3 +29,4 @@ from .PatchDynamics import PatchFlow, PatchComposition
 from .Export import Export
 from .LinuxMailCharacteristics import LinuxMailCharacteristics,\
     load_linux_mail_characteristics
+from .LinuxMaintainers import load_maintainers
