9ac5213d5e602ac22adb97a869fe7d8dfc9afd28
Ralf Ramsauer <ralf.ramsauer@oth-regensburg.de>
2020-08-31 18:32:24 +0200

    pypasta: Linux: move get_relevant_patches to Linux.py
    to make it available for other users.
    
    Signed-off-by: Ralf Ramsauer <ralf.ramsauer@oth-regensburg.de>

--- pypasta/Linux.py
+++ pypasta/Linux.py
@@ -10,8 +10,12 @@ This work is licensed under the terms of the GNU GPL, version 2.  See
 the COPYING file in the top-level directory.
 """
 
+from logging import getLogger
+
 from .LinuxMailCharacteristics import load_linux_mail_characteristics
 from .LinuxMaintainers import load_maintainers
+
+log = getLogger(__name__[-15:])
 
 
 def load_characteristics_and_maintainers(config, clustering):
@@ -35,3 +39,86 @@ def load_characteristics_and_maintainers(config, clustering):
 
     return characteristics, maintainers_version
 
+
+def get_relevant_patches(characteristics):
+    # First, we have to define the term 'relevant patch'. For our analysis, we
+    # must only consider patches that either fulfil rule 1 or 2:
+    #
+    # 1. Patch is the parent of a thread.
+    #    This covers classic one-email patches
+    #
+    # 2. Patch is the 1st level child of the parent of a thread
+    #    In this case, the parent can either be a patch (e.g., a series w/o
+    #    cover letter) or not a patch (e.g., parent is a cover letter)
+    #
+    # 3. The patch must not be sent from a bot (e.g., tip-bot)
+    #
+    # 4. Ignore stable review patches
+    #
+    # All other patches MUST be ignored. Rationale: Maintainers may re-send
+    # the patch as a reply of the discussion. Such patches must be ignored.
+    # Example: Look at the thread of
+    #     <20190408072929.952A1441D3B@finisterre.ee.mobilebroadband>
+    #
+    # Furthermore, only consider patches that actually patch Linux (~14% of all
+    # patches on Linux MLs patch other projects). Then only consider patches
+    # that are not for next, not from bots (there are a lot of bots) and that
+    # are no 'process mails' (e.g., pull requests)
+
+    relevant = set()
+
+    all_messages = 0
+    skipped_bot = 0
+    skipped_stable = 0
+    skipped_not_linux = 0
+    skipped_no_patch = 0
+    skipped_not_first_patch = 0
+    skipped_process = 0
+    skipped_next = 0
+
+    for m, c in characteristics.items():
+        skip = False
+        all_messages += 1
+
+        if not c.is_patch:
+            skipped_no_patch += 1
+            continue
+
+        if not c.patches_linux:
+            skipped_not_linux += 1
+            skip = True
+        if not c.is_first_patch_in_thread:
+            skipped_not_first_patch += 1
+            skip = True
+
+        if c.is_from_bot:
+            skipped_bot += 1
+            skip = True
+        if c.is_stable_review:
+            skipped_stable += 1
+            skip = True
+        if c.process_mail:
+            skipped_process += 1
+            skip = True
+        if c.is_next:
+            skipped_next += 1
+            skip = True
+
+        if skip:
+            continue
+
+        relevant.add(m)
+
+    log.info('')
+    log.info('=== Calculation of relevant patches ===')
+    log.info('All messages: %u' % all_messages)
+    log.info('  No patches: %u' % skipped_no_patch)
+    log.info('Skipped patches:')
+    log.info('  Not Linux: %u' % skipped_not_linux)
+    log.info('  Bot: %u' % skipped_bot)
+    log.info('  Stable: %u' % skipped_stable)
+    log.info('  Process mail: %u' % skipped_process)
+    log.info('  Next: %u' % skipped_next)
+    log.info('Relevant patches: %u' % len(relevant))
+
+    return relevant
