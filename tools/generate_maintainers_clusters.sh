286b65f4fb018eed46f706d46a415ef9954e85a8
Ralf Ramsauer <ralf.ramsauer@oth-regensburg.de>
2021-09-06 18:28:28 +0200

    tools: generate_maintainers_cluster: improve script
    Signed-off-by: Ralf Ramsauer <ralf.ramsauer@oth-regensburg.de>

--- tools/generate_maintainers_clusters.sh
+++ tools/generate_maintainers_clusters.sh
@@ -10,6 +10,18 @@
 
 project=$(cat config)
 tags=$(git -C resources/$project/repo tag --list)
+repo="./resources/$project/repo"
+
+if [[ $project == "linux" ]]; then
+	tags=$(git -C $repo tag --list | grep -v "^v2\.6\.")
+elif [[ $project == "u-boot" ]]; then
+	tags=$(git -C $repo tag --list | grep "^v20[12]" | grep -v "^v201[0123]" | grep -v "^v2014\.0[147]" | grep -v "v2014\.10-rc1")
+elif [[ $project == "xen" ]]; then
+	tags=$(git -C $repo tag --list | grep -v "^3" | grep -v "^4\.0\." | grep -v "RELEASE-[23]" | grep -P "^\d|R")
+elif [[ $project == "qemu" ]]; then
+	tags=$(git -C $repo tag --list | grep -v "^release" | grep -v "^v0\.[123456789]\." | grep -v "^v0\.1[0123]\.")
+	echo $tags
+fi
 
 RES="resources/$project/resources"
 CLSTRS=$RES/maintainers_cluster
