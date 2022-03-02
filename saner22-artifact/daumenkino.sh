877dc77e32eee75d1f7e9a9b26e0a8adbd936b3f
Ralf Ramsauer <ralf.ramsauer@oth-regensburg.de>
2022-03-02 15:50:36 +0100

    daumenkino -> animate_test.R

--- saner22-artifact/daumenkino.sh
+++ saner22-artifact/daumenkino.sh
@@ -12,26 +12,27 @@ set -e
 
 project=$1
 
-daumenkino="$HOME/results/daumenkino/$project"
-template="./saner22-artifact/tikz_template.tex"
-build="$daumenkino/build"
-
-mkdir -p $daumenkino $build
-
 ./pasta set_config $project
+./analyses/animate_test.R
 
-realpath ./resources/$project/resources/maintainers_section_graph/* | grep -v -- "-rc" | grep -v "filemap" | \
-	xargs -n 1 -I {} -P $(nproc) bash -c "./analyses/tex_generator.R {} \$(echo {} | sed -e 's/\.csv/_filemap\.csv/')"
+#daumenkino="$HOME/results/daumenkino/$project"
+#template="./saner22-artifact/tikz_template.tex"
+#build="$daumenkino/build"
 
-for i in resources/$project/resources/R/graphdesc*.tex; do
-	./saner22-artifact/daumenkino/daumenkino.py -i $i -o $daumenkino/$(basename $i)
-done
+#mkdir -p $daumenkino $build
 
-ls $daumenkino/*.tex | \
-	xargs -n 1 -I {} -P $(nproc) \
-	bash -c "lualatex -output-directory $build -jobname \$(basename -s .tex {}) \"\\newcommand{\\version}{\$(basename -s .tex {} | cut -b 11-)}\\newcommand{\\filename}{{}}\\input{$template}\""
-
-mv -v $build/*.pdf $daumenkino/
-
-files=$(ls $daumenkino/*.pdf | sort -V)
-pdftk $files cat output $daumenkino/daumenkino.pdf
+#realpath ./resources/$project/resources/maintainers_section_graph/* | grep -v -- "-rc" | grep -v "filemap" | \
+#	xargs -n 1 -I {} -P $(nproc) bash -c "./analyses/tex_generator.R {} \$(echo {} | sed -e 's/\.csv/_filemap\.csv/')"
+#
+#for i in resources/$project/resources/R/graphdesc*.tex; do
+#	./saner22-artifact/daumenkino/daumenkino.py -i $i -o $daumenkino/$(basename $i)
+#done
+#
+#ls $daumenkino/*.tex | \
+#	xargs -n 1 -I {} -P $(nproc) \
+#	bash -c "lualatex -output-directory $build -jobname \$(basename -s .tex {}) \"\\newcommand{\\version}{\$(basename -s .tex {} | cut -b 11-)}\\newcommand{\\filename}{{}}\\input{$template}\""
+#
+#mv -v $build/*.pdf $daumenkino/
+#
+#files=$(ls $daumenkino/*.pdf | sort -V)
+#pdftk $files cat output $daumenkino/daumenkino.pdf
