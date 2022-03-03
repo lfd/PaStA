fe5924debe7a16e748630a21c3d3d6357b48bfb8
Pia Eichinger <pia.eichinger@st.oth-regensburg.de>
2022-03-03 16:56:58 +0100

    add cluster metrics to descriptive data script

--- fse22-artifact/LoCMaintainersScript.sh
+++ fse22-artifact/LoCMaintainersScript.sh
@@ -7,7 +7,7 @@ do
 	cd "resources/$project/repo"
 	git fetch --all
 	if [[ $project == "linux" ]]; then
-		versions=$(git tag --list | grep -E "^v[0-9]{1,2}\.[0-9]{1,3}$" | sort -V)
+		versions=$(git tag --list | grep -E "(^v2\.6\.39$|^v[0-9]{1,2}\.[0-9]{1,3}$)" | sort -V)
 	elif [[ $project == "u-boot" ]]; then
 		versions=$(git tag --list | grep -E "^v20[12]" | grep "^v20[12]" | grep -v "^v201[0123]" | grep -v "^v2014\.0[147]" | grep -v "v2014\.10-rc1" | sort -V)
 	elif [[ $project == "xen" ]]; then
@@ -16,12 +16,14 @@ do
 		versions=$(git tag --list | grep -v "^release" | grep -v "^v0\.[123456789]\." | grep -v "^v0\.1[0123]\." | grep -v "initial")
 	fi
 	
-	echo "Project; Version; No-Mtrs; LoC"
+	echo "Project; Version; No-Mtrs; LoC; No-Sections; No-Clusters"
 	for version in $versions; do
 	        git checkout $version > /dev/null 2>&1
 	        mtrs=$(cat MAINTAINERS | grep -a "^M:" | sort | uniq | wc -l)
 	        loc=$(find . -type f -not -regex '\./\.git.*'|xargs cat|wc -l)
-	        echo "$project; $version; $mtrs; $loc"
+		sections=$(cat ../resources/maintainers_section_graph/$version.csv | cut -d, -f1 | uniq | wc -l)
+		clusters=$(cat ../resources/maintainers_cluster/$version.csv | cut -d, -f1 | uniq | wc -l)
+		echo "$project; $version; $mtrs; $loc; $(($sections-1)); $(($clusters-1))"
 	done
 	cd -
 done
