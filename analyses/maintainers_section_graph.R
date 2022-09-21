c4d0d0807474cb1febfbdd0859e581fe26c659ff
Pia Eichinger <pia.eichinger@st.oth-regensburg.de>
2022-09-21 17:03:13 +0200

    analyses: maintainers_section_graph: removing old comments

--- analyses/maintainers_section_graph.R
+++ analyses/maintainers_section_graph.R
@@ -128,7 +128,6 @@ if (PRINT_ENTIRE_GRAPH) {
               )
 
   Isolated = which(degree(g)==0)
-  #TODO: das iwie rechtfertigen, weil es schaut hald echt schlecht aus
   g = delete.vertices(g, Isolated)
 
   V(g)$clu <- as.character(membership(cluster_walktrap(g)))
@@ -136,16 +135,12 @@ if (PRINT_ENTIRE_GRAPH) {
   quantiles <- quantile(V(g)$size, c(.75, .80, .85, .90, .95, .98, .99)) 
   p <- ggraph(g, layout = "dh") +
   geom_edge_link0(aes(edge_width = weight),edge_colour = "grey66")+
-  #geom_node_point(aes(fill = clu,size = size),shape = 21)+
   geom_node_point(aes(fill = clu, size = size),shape = 21)+
   geom_node_text(aes(filter = size >= unname(quantiles[quantile_step]), label = name),family="serif", size=2)+
-  #geom_node_text(aes(label = name),family="serif", size=2)+
-  #scale_fill_manual(values = got_palette)+
   scale_edge_width(range = c(0.2,3))+
   scale_size(range = c(1,6))+
   theme_graph(base_family = "Helvetica")+
   theme(legend.position = "none")
-  #ggtitle(project)
   
   printplot(p, "microview")
 }
@@ -169,18 +164,12 @@ if (PRINT_CLUSTERS) {
     V(cluster_graph)$category <- sample(LETTERS, length(V(cluster_graph)), T)
     cluster_graph <- ggraph(cluster_graph, layout = "dh") +
     geom_edge_link0(aes(edge_width = weight),edge_colour = "grey66")+
-    #geom_node_point(aes(fill = clu,size = size),shape = 21)+
     geom_node_point(aes(fill = category, size = size),shape = 21)+
-    #geom_node_text(aes(filter = size >= unname(quantiles[quantile_step]), label = name),family="serif", size=4)+
     geom_node_text(aes(label = name),family="serif", size=NETWORK_SIZE, repel=TRUE)+
-    #scale_fill_manual(values = got_palette)+
     scale_edge_width(range = c(0.2,3))+
     scale_size(range = c(1,6))+
     theme_graph(base_family = "Helvetica")+
     theme(legend.position = "none")
-    #ggtitle(graph_name)
-
-    #wt_clusters <- cluster_walktrap(cluster_graph)
 
     printplot(cluster_graph, paste0("cluster_", toString(i)))
   }
@@ -188,69 +177,3 @@ if (PRINT_CLUSTERS) {
 
 write_cluster_csv(graph_list, CLUSTER_DESTINATION)
 
-
-
-#for (i in bounds) {
-#group_1 <- comm_groups[indexes[1]]
-#group_list_1 <- unname(group_1)[[1]]
-#cluster_graph_1 <- igraph::delete_vertices(g, which(!(V(g)$name %in% group_list_1)))
-#V(cluster_graph_1)$category <- sample(LETTERS, length(V(cluster_graph_1)), T)
-#  
-#}
-#
-#indexes <- c(25, 58)
-#g <- graph_list$g
-#comm_groups <- graph_list$comm_groups
-#
-#group_1 <- comm_groups[indexes[1]]
-#group_list_1 <- unname(group_1)[[1]]
-#cluster_graph_1 <- igraph::delete_vertices(g, which(!(V(g)$name %in% group_list_1)))
-#V(cluster_graph_1)$category <- sample(LETTERS, length(V(cluster_graph_1)), T)
-#
-#group_2 <- comm_groups[indexes[2]]
-#group_list_2 <- unname(group_2)[[1]]
-#cluster_graph_2 <- igraph::delete_vertices(g, which(!(V(g)$name %in% group_list_2)))
-#V(cluster_graph_2)$category <- sample(LETTERS, length(V(cluster_graph_2)), T)
-#V(cluster_graph_2)$clu <- "black"
-#
-#
-#ggraph(cluster_graph_1, layout = "dh") +
-#geom_edge_link0(aes(edge_width = weight),edge_colour = "grey66")+
-##geom_node_point(aes(fill = clu,size = size),shape = 21)+
-#geom_node_point(aes(fill = category, size = size),shape = 21)+
-##geom_node_text(aes(filter = size >= unname(quantiles[quantile_step]), label = name),family="serif", size=4)+
-#geom_node_text(aes(label = name),family="serif", size=4, repel=TRUE)+
-##scale_fill_manual(values = got_palette)+
-#scale_edge_width(range = c(0.2,3))+
-#scale_size(range = c(1,6))+
-#theme_graph(base_family = "Helvetica")+
-#theme(legend.position = "none")
-##ggtitle(graph_name)
-#
-## TODO: make it reproducible to extract security subsystem
-#filename <- file.path("resources/R", "security_subsystem")
-#WIDTH <- 4
-#HEIGHT <- 3
-#tikz(paste0(filename, '.tex'), width = WIDTH, height = HEIGHT, sanitize=TRUE, standAlone = FALSE)
-#
-#ggraph(cluster_graph_2, layout = "stress") +
-##geom_edge_link0(aes(edge_width = 0.25),edge_colour = "grey66")+
-#geom_edge_link0(aes(edge_width = weight),edge_colour = "grey66")+
-##geom_node_point(aes(fill = "black",size = size, colour="black"),shape = 21, color="black")+
-##geom_node_point(aes(size=0.5, fill="white"))+
-##geom_node_point(aes(fill = "black", size = size),shape = 21)+
-##geom_node_text(aes(filter = size >= unname(quantiles[quantile_step]), label = name),family="serif", size=4)+
-##geom_node_text(aes(label = name),family="serif", size=2.75, repel=FALSE)+
-#geom_label(aes(x = x, y = y, label = name), nudge_y = 0, label.size = NA) +
-##scale_fill_manual(values = got_palette)+
-##scale_edge_width(range = c(0.2,3))+
-#scale_edge_width(range = c(1.2,3))+
-#scale_size(range = c(1,3))+
-#theme_graph(base_family = "Helvetica")+
-#  ylim(-1.5, 1.5) +
-#  xlim(-1.5, 1.5) +
-#theme(legend.position = "none")
-##ggtitle(graph_name)
-#dev.off()
-#  
-#  
